0685b00ba33ea32a87b0371941b0c65980dd5670
Christopher R. Palmer <crpalmer@gmail.com>
2017-04-19 06:02:37 -0400

    mm: Properly initialize shrink_control when showing shrinker debug
    In particular, prior to this change nid was not initialized which
    would potentially crash the kernel when attempting to show the
    count from a numa aware shrinker such as the one in fs.
    
    Change-Id: I250989bd53b90ba3fcdbcb7da10210b772f145cf

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -207,8 +207,8 @@ static int debug_shrinker_show(struct seq_file *s, void *unused)
 	struct shrinker *shrinker;
 	struct shrink_control sc;
 
+	memset(&sc, 0, sizeof(sc));
 	sc.gfp_mask = -1;
-	sc.nr_to_scan = 0;
 
 	down_read(&shrinker_rwsem);
 	list_for_each_entry(shrinker, &shrinker_list, list) {
